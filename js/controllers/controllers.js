0ed6598c74df29d3fd9c5b993f3233d0970ec957
Shaheen Ghazazani <shaheenghazazani@gmail.com>
2015-12-21 19:33:40 -0500

    Update controller.js
    organization is key

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -1,11 +1,12 @@
-(function(){ 	
+(function(){ 
+//basic async JS	
 	var leaderboardApp = angular.module('leaderboardApp', [])
 	.controller('challengerController', function($scope, $http) {
   	var LoLAPIkey = '15b1f631-0414-4515-a175-927e7f5807b8'; //see developer.riotgames.com for ref
 
   	//lets get the ranked solo 5x5 ladder board
   	$http.get('https://na.api.pvp.net/api/lol/na/v2.5/league/challenger?type=RANKED_SOLO_5x5&api_key='+LoLAPIkey).success(function(data) {
-    	$scope.PlayersRanking = data.entries;
+    		$scope.PlayersRanking = data.entries;
   	});
 
   	//lets see if NA is alive even..
@@ -16,5 +17,4 @@
   	// Sort players from the heightest leaguePoints to the lowest.
 	$scope.pointPrioritization = '-leaguePoints';
 	});
-  	//wow this is dope
 })();
